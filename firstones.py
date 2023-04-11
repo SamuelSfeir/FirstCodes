9430295d311b57d4902e23a9753b8bc80a8f227b
SamuelSfeir <128386482+SamuelSfeir@users.noreply.github.com>
2023-04-11 10:54:10 -0300

    Update firstones.py

--- firstones.py
+++ firstones.py
@@ -1,1 +1,7 @@
-
+while True:
+    user_input = input("Type a number to divide by two or write 'out' to get out: ")
+    if user_input == "out" or user_input == "Out" or user_input == "OUT":
+        print("Gotcha! Taking you out")
+        break
+    number = int(user_input)
+    print('There you go:',number/2)
