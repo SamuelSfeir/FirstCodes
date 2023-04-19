57b2f12b3eaab86ccaf6dbbb75332c6d19297447
SamuelSfeir <128386482+SamuelSfeir@users.noreply.github.com>
2023-04-19 10:34:20 -0300

    Update EstoqueBast.py

--- EstoqueBast.py
+++ EstoqueBast.py
@@ -1,11 +1,10 @@
 while True:
+    print('Hi! Welcome to my stock control. Lets get started!''\n')
     user_input3 = input('Quantos bastões tem em estoque: ')
     user_input = input('Quantas vendas teve de 30: ')
     user_input1 = input('Quantas vendas teve de 20: ')
     user_input2 = input('Quantas vendas teve de 10: ')
     question = input('Quer saber quantos bastões foram gastos? (s/n): ')
-    if user_input == 'done':
-        break
     user_inputint = int(user_input)
     user_inputint1 = int(user_input1)
     user_inputint2 = int(user_input2)
@@ -14,12 +13,17 @@ while True:
     bast_20 = user_inputint1 * 20
     bast_10 = user_inputint2 * 10
     estoque_bast = user_inputint3 - (bast_10 + bast_20 + bast_30)
-    if question == "Yes" or question == "yes" or question == 'Sim' or question == 'sim' or question == 'y' or question == 'Y' or question == 'S' or question == 's':
+    if question.lower() in ["yes",'sim','y','s']:
             gastos = bast_10 + bast_20 + bast_30
             print('Seu estoque de bastão agora é:', estoque_bast, '\n''E hoje foram gastos:', gastos, 'bastões')
-            break
-    elif question == 'n':
+    elif question.lower() in ['no','n','não','nao']:
         print('Seu estoque de bastão agora é:', estoque_bast)
     else:
-        print('I am sorry, I am a beginner in programming, in order to continue the program, please type either:''\n''(s or n)')
+        print('I am sorry, I am a beginner in programming, in order to continue the program, please type either:''\n''(s/sim or n/nao)')
+        continue
+    again_question = input('Do you want to do it one more time? ')
+    if again_question.lower() in ['no','n','não','nao']:
+        print('Okay! Thanks for your time! Bye bye see you soon :)')
+        break
+    elif again_question.lower() in ['yes','y','sim','s']:
         continue
