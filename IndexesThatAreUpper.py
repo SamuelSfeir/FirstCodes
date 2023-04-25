c1100e95fe8d5520f4030032fdb2fa3dadb69a5d
SamuelSfeir <128386482+SamuelSfeir@users.noreply.github.com>
2023-04-25 10:14:15 -0300

    Update IndexesThatAreUpper.py

--- IndexesThatAreUpper.py
+++ IndexesThatAreUpper.py
@@ -1,6 +1,29 @@
+# I did this python challenge that requested a program that return the indexes that have capital letters.
+
+# So, first I did this following one:
+
 def capital_indexes(x):
     z = 0
     for i in x:
         z = z + 1
         if i.isupper():
             print(z-1)
+            
+# Then I did this one:
+
+def capital_indexes(x):
+    for i in x:
+        z = 0
+        if i in ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']:
+            z = z + 1
+            return i,z-1
+        
+# And finally I did this one, which in my opinion, is the best one:
+
+def capital_indexes(x):
+    for index,letter in enumerate(x):
+        if letter.isupper():
+            print(index)
+
+            
+    
