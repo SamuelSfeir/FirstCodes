42b5aeaec4af4652f60d2533c7fbc73f5e3f0fad
SamuelSfeir <128386482+SamuelSfeir@users.noreply.github.com>
2023-04-28 10:19:57 -0300

    Create RemoveAddDots.py

---
+++ RemoveAddDots.py
@@ -0,0 +1,17 @@
+# The goal of this challenge was to create a program that returns a string that has dots between the letters without dots
+# And the opposite as well (if the string does not have dots, it will return with dots)
+
+# So I thougth about two ways of doing that. As the first way, we have:
+
+def add_dots(string):
+    return ".".join([letter for letter in string])
+def remove_dots(string):
+    return string.replace(".","")
+  
+# As for the second way of doing it, we have:
+def add_dots(string):
+    for letter in string[:-1]:
+        print(letter, end=".")
+    return (string[-1])
+def remove_dots(string):
+    return string.replace(".","")
